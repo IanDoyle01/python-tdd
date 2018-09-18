cec3c8dd8e1940b8c90174338c4d1797aa49ecd2
Ian Doyle <iandoyle01@gmail.com>
2018-09-18 16:31:46 +0000

    added functions and comments

--- evens.py
+++ evens.py
@@ -1,3 +1,13 @@
+#helper function to check if a number is even or not
+def is_even(number):
+    return number % 2 == 0
+
+def even_number_of_evens(numbers):
+    #list comprehension to clean up code
+    evens = sum([1 for n in numbers if is_even(n)]) #add +1 to evens for each number if number is even
+    return False if evens == 0 else is_even(evens) #return false unless evens is even
+
+#Tests
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2]) == False, "One even number"
 assert even_number_of_evens([2, 4]) == True, "Two even numbers"
