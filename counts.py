2327f872b5f6cac09b530e6f5248eeb0666fb2a2
Ian Doyle <iandoyle01@gmail.com>
2018-09-18 15:52:32 +0000

    added more complex tests

--- counts.py
+++ counts.py
@@ -9,5 +9,6 @@ assert count_upper_case("") == 0, "Empty string"
 assert count_upper_case("A") == 1, "1 upper case"
 assert count_upper_case("a") == 0, "1 lower case"
 assert count_upper_case("£$%%^") == 0, "Special Characters"
-
+assert count_upper_case("AaAB") == 3, "3 upper and 1 lower case"
+assert count_upper_case("A£$bC") == 2, "2 upper with lower case and special chars"
 print("All the tests passed")
