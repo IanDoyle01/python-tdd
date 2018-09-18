cc97e563d92849bdcdfbc38320b9bfc579ae0798
Ian Doyle <iandoyle01@gmail.com>
2018-09-18 15:49:43 +0000

    added tests

--- counts.py
+++ counts.py
@@ -3,4 +3,11 @@ def count_upper_case(message):
     for c in message:
         if c.isupper():
             count += 1
-        return count
+    return count
+        
+assert count_upper_case("") == 0, "Empty string"
+assert count_upper_case("A") == 1, "1 upper case"
+assert count_upper_case("a") == 0, "1 lower case"
+assert count_upper_case("£$%%^") == 0, "Special Characters"
+
+print("All the tests passed")
