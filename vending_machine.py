f855f78380ce82e4e6440f17b667046e25617255
Ian Doyle <iandoyle01@gmail.com>
2018-09-18 17:36:19 +0000

    added test for multiples of same coin and adjusted function

--- vending_machine.py
+++ vending_machine.py
@@ -9,7 +9,7 @@ def get_change(amount):
     
     change = []
     for coin in [200, 100, 50, 20, 10, 5, 2, 1]:
-        if coin <= amount:
+        while coin <= amount:
             amount -= coin
             change.append(coin)
             
@@ -26,5 +26,6 @@ test_are_equal(get_change(100),[100])
 test_are_equal(get_change(200),[200])
 test_are_equal(get_change(3),[2, 1])
 test_are_equal(get_change(7),[5, 2])
+test_are_equal(get_change(9),[5, 2, 2])
 
 print("All tests passed")
