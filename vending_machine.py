82e0089393fdff32e5b072af43543a28dd028bd6
Ian Doyle <iandoyle01@gmail.com>
2018-09-19 05:27:36 +0000

    refactored to dictionary format to count available coins

--- vending_machine.py
+++ vending_machine.py
@@ -1,18 +1,20 @@
 from byotest import *
 
-usd_coins = [100, 50, 25, 10, 5, 1] #US Dollar Coin Denominations
-eur_coins = [200, 100, 50, 20, 10, 5, 2, 1] #Euro Coin Denominations
+usd_coins = {100:20, 50:20, 25:20, 10:20, 5:20, 1:20} #US Dollar Coin Denominations
+eur_coins = {200:20, 100:20, 50:20, 20:20, 10:20, 5:20, 2:20, 1:20} #Euro Coin Denominations
 
 def get_change(amount, coins=eur_coins): #defaults to euro coins
     
     change = [] #empty list to hold change
-    
-    for coin in coins:
-        while coin <= amount:
-            amount -= coin
-            change.append(coin)
-            
+    for denomination in sorted(coins.keys(), reverse=True): #sorted because dicts dont keep order
+        while denomination <= amount and coins[denomination] > 0:
+            amount -= denomination
+            coins[denomination] -= 1
+            change.append(denomination)
+    if amount != 0:
+        raise Exception("Insufficient coins to give change.")
     return change
+
 
 #Tests
 test_are_equal(get_change(0),[])
@@ -28,5 +30,8 @@ test_are_equal(get_change(3),[2, 1])
 test_are_equal(get_change(7),[5, 2])
 test_are_equal(get_change(9),[5, 2, 2])
 test_are_equal(get_change(35, usd_coins),[25,10])
+test_are_equal(get_change(35), [20, 10, 5])
+test_are_equal(get_change(5, {2: 1, 1: 4}), [2, 1, 1, 1])
+test_exception_was_raised(get_change, (5, {2: 1, 1: 2}), "Insufficient coins to give change.")
 
 print("All tests passed")
